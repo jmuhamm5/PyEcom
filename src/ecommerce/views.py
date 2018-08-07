d8c41f39bf29f5397a779690891731f3eb7e4bb7
Jamil Muhammad <apple_life15@yahoo.com>
2018-08-06 20:12:30 -0500

    templates, settings, contact form

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -3,4 +3,22 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    context = {
+        'title' : 'Hello World',
+        'content' : 'Welcome to the homepage.'
+    }
+    return render(request, 'pages/home.html', context)
+
+def about_page(request):
+    context = {
+        'title' : 'About Page',
+        'content' : 'Welcome to the about page.'
+    }
+    return render(request, 'pages/about.html', context)
+
+def contact_page(request):
+    context = {
+        'title' : 'Contact Page',
+        'content' : 'Welcome to the contact page.'
+    }
+    return render(request, 'pages/contact.html', context)
