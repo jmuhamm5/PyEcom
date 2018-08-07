8aa4e9697fed642b200f16bdf22b0f8eb2035f48
Jamil Muhammad <apple_life15@yahoo.com>
2018-08-07 01:23:22 -0500

    django forms and user login

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import ContactForm, LoginForm
 
 
 def home_page(request):
@@ -17,8 +20,47 @@ def about_page(request):
     return render(request, 'pages/about.html', context)
 
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
         'title' : 'Contact Page',
-        'content' : 'Welcome to the contact page.'
+        'content' : 'Welcome to the contact page.',
+        'form' : contact_form
     }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == "POST":
+    #     #print(request.POST)
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
     return render(request, 'pages/contact.html', context)
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form" : form
+    }  
+    #print(request.user.is_authenticated())
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        #print(request.user.is_authenticated())
+        if user is not None:
+            #print(request.user.is_authenticated())
+            login(request, user)
+            # Redirect to a success page.
+            # context['form'] = LoginForm()
+            return redirect("/login")
+        else:
+            # Return an 'invalid login' error message.
+            print('error')       
+    return render(request, "pages/auth/login.html", context)
+
+def register_page(request):
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+    return render(request, "pages/auth/register.html", {})
