336abd516a871d2b2f5fa393c43573897788ad0d
Jamil Muhammad <apple_life15@yahoo.com>
2018-08-15 18:35:16 -0500

    creating the search form

--- src/search/urls.py
+++ src/search/urls.py
@@ -5,5 +5,5 @@ from .views import SearchProductListView
 app_name = "search" # django 2 feature for namespace to work in main url file
 
 urlpatterns = [
-    path('', SearchProductListView.as_view(), name='list')
+    path('', SearchProductListView.as_view(), name='query')
 ] 
