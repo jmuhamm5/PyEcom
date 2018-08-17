755ba3ef701f9c4735ba246aaab3503f2b377ff8
Jamil Muhammad <apple_life15@yahoo.com>
2018-08-17 04:46:40 -0500

    cart model manager part1

--- src/carts/models.py
+++ src/carts/models.py
@@ -7,16 +7,26 @@ from products.models import Product
 
 User = settings.AUTH_USER_MODEL
 
+class CartManager(models.Manager):
+    def new(self, user=None):
+        user_obj = None
+        if user is not None:
+            if user.is_authenticated:
+                user_obj = user
+        return self.model.objects.create(user=user_obj)
+
 class Cart(models.Model):
-    user        = models.ForeignKey(User, on_delete=models.CASCADE,null=True, blank=True)
-    product     = models.ManyToManyField(Product, blank=True)
+    user        = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    products    = models.ManyToManyField(Product, blank=True)
     total       = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
-    updated     = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
 
+    objects = CartManager()
+
     def __str__(self): # Python 3
-        return self.id
+        return str(self.id)
     
     def __unicode__(self): # Python 2
-        return self.id
+        return str(self.id)
 
--- src/carts/views.py
+++ src/carts/views.py
@@ -4,23 +4,15 @@ from .models import Cart
 
 # Create your views here.
 
-def cart_create(user=None):
-    cart_obj = request.session.get("cart_id", None)
-    print('new cart created')
-    return cart_obj
-
 def cart_home(request):
-    request.session['cart_id'] = "12"
     cart_id = request.session.get("cart_id", None)
-    # if cart_id is None:
-    #     cart_obj = cart_create()
-    #     request.session['cart_id'] = cart_obj.id
-    # else:
-    qs = Cart.objects.get(id=cart_id)
+    qs = Cart.objects.filter(id=cart_id)
     if qs.count() == 1:
-        print('Cart id exists')
         cart_obj = qs.first()
+        if request.user.is_authenticated and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
     else:
-        cart_obj = cart_create()
+        cart_obj = Cart.objects.new(user=request.user)
         request.session['cart_id'] = cart_obj.id   
     return render(request, "carts/home.html", {})
