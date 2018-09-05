29ea56df9957dedf32d65d674988e46b62dee27b
Jamil Muhammad <apple_life15@yahoo.com>
2018-09-05 00:44:23 -0500

    checkout view

--- src/carts/urls.py
+++ src/carts/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from .views import cart_home, cart_update
+from .views import (
+    cart_home, 
+    cart_update, 
+    checkout_home
+    )
 
 app_name = "cart" # django 2 feature for namespace to work in main url file
 
 urlpatterns = [
     path('', cart_home, name='home'),
+    path('checkout/', checkout_home, name='checkout'),
     path('update/', cart_update, name='update')
 ] 
